e2226b82d376180aeb8060cccaab76ab3837e367
Dan Andrei <andreidani96@gmail.com>
2018-04-12 12:46:38 +0300

    first commit

---
+++ config/environments/production.js
@@ -0,0 +1,10 @@
+const nconf = require('nconf');
+
+nconf.set('database', {
+  server: '127.0.0.1',
+  dbName: 'car-booking-production',
+});
+
+nconf.set('secrets', {
+  jwtSecret: 'rgxVDSWP9jhMAyBWEgqjhuejlGKB2Wb9cwZEXxg8',
+});
